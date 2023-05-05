d72bbeceb93a2a9ff2f8559b442326d5b8217595
Sobhan Etaati <etaatisobhan@gmail.com>
2023-05-05 12:26:13 +0330

    sign up api commit

--- http_monitoring/api/admin.py
+++ http_monitoring/api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import CustomUsers
+from .models import CustomUsers, URLs
 
 # Register your models here.
 admin.site.register(CustomUsers)
+admin.site.register(URLs)
--- http_monitoring/api/views.py
+++ http_monitoring/api/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .serializers import CustomUserSerializer, URLSerializer
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.request import Request
+
 
 # Create your views here.
+
+class SignupView(generics.GenericAPIView):
+    serializer_class = CustomUserSerializer
+
+    def post(self, request: Request):
+        data = request.data
+
+        serializer = self.serializer_class(data = data)
+
+        if serializer.is_valid():
+            serializer.save()
+            response = {
+                        "message": "User Created Successfully", 
+                        "data":serializer.data
+                        }
+            
+            return Response(data=response, status=status.HTTP_201_CREATED)
+        
+        return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
+        
